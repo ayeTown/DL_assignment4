882cff407c5cd038441e888d58f6b902d46cf18d
Alice Townsend <al1c3@outlook.com>
2021-04-19 17:56:13 -0400

    nana

--- assignment4.py
+++ assignment4.py
@@ -3,56 +3,74 @@ import time
 import threading
 import pandas as pd
 import torch
+import sys
 from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
-import torch.nn.functional as F
-import torch.multiprocessing as mp
-import torch.distributed.rpc as rpc
-from torch.distributed.rpc import RRef
 import torch.distributed.autograd as dist_autograd
-from torch.distributed.optim import DistributedOptimizer
 import matplotlib.pyplot as plt
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.distributed import DistributedSampler
+from torchvision.datasets import MNIST
+from torchvision import transforms
+from time import perf_counter
 
 
-class NeuralNetwork(nn.Module):
-    def __init__(self,num_classes = 10):
-        super(NeuralNetwork, self).__init__()
+#class NeuralNetwork(nn.Module):
+#    def __init__(self,num_classes = 10):
+#        super(NeuralNetwork, self).__init__()
+#        self.num_classes = num_classes
+#        self.conv_stack = nn.Sequential(
+#            nn.Conv2d(1,10,kernel_size = 4),
+#            nn.ReLU(),
+#            nn.MaxPool2d(kernel_size = 5,stride = 5),
+#            nn.Conv2d(10,10,kernel_size = 2),
+#            nn.ReLU(),
+#            nn.MaxPool2d(kernel_size = 2,stride = 2),
+#            nn.Flatten(),
+#            nn.Linear(40,num_classes),
+#            nn.ReLU()
+#        )
+#    def forward(self, x):
+#        logits = self.conv_stack(x)
+#        return logits
+
+class VolModel(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
         self.num_classes = num_classes
-        self.conv_stack = nn.Sequential(
-            nn.Conv2d(1,10,kernel_size = 4),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 5,stride = 5),
-            nn.Conv2d(10,10,kernel_size = 2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2,stride = 2),
-            nn.Flatten(),
-            nn.Linear(40,num_classes),
-            nn.ReLU()
-        )
+
+        self.conv1 = nn.Conv2d(1, 4, 3)
+        self.activation = nn.ReLU()
+        self.conv2 = nn.Conv2d(4, 10, 3)
+        self.pool = nn.AdaptiveMaxPool2d((1, 1))
+        self.classifier = nn.Linear(10, num_classes)
+
     def forward(self, x):
-        logits = self.conv_stack(x)
-        return logits
+        x = self.conv1(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+        x = self.activation(x)
+        x = self.pool(x)
+        x = x.squeeze(-1).squeeze(-1)
+        x = self.classifier(x)
+        return x
 
-
-def train(num_epochs,batch_size,learning_rate):
+def train(num_epochs = 30,batch_size = 1,learning_rate = 0.05):
     training_data = datasets.MNIST(
-        root = "data",
+        root = ".",
         train = True,
-        download = True,
-        transform = ToTensor(),
-    )
-    test_data = datasets.MNIST(
-        root = "data",
-        train = False,
-        download = True,
-        transform = ToTensor(),
+        download = False,
+        transform = transforms.Compose([transforms.ToTensor()])
     )
     sampler = DistributedSampler(training_data)
     training_dataloader = DataLoader(training_data,batch_size = batch_size,sampler = sampler)
-    model = NeuralNetwork(num_classes = 10)
+    #model = NeuralNetwork(num_classes = 10)
+    model = VolModel(num_classes=10)
     model = DistributedDataParallel(model)
     rank = dist.get_rank()
     world_size = dist.get_world_size()
@@ -83,7 +101,7 @@ def train(num_epochs,batch_size,learning_rate):
         elapsed_times.append(perf_counter()  - start)
         metrics = pd.DataFrame({'epoch_losses': epoch_losses, 'elapsed_time':
         elapsed_times})
-        metrics.to_csv('metrics.csv')
+        metrics.to_csv('/lustre/haven/proj/UTK0150/atown/assignment4/metrics_8P.csv')
     return iter_losses, epoch_losses
 
 
@@ -101,4 +119,6 @@ if __name__ ==  '__main__':
         world_size = world_size,
         rank = rank,
     )
+    print("Started training")
     train()
+    print("Ended training")
